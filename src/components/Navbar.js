ae77993e968a1ff3df10a3f47646a1f82a0dae33
Jean-Pierre Novak <jeanpierrenovak21@gmail.com>
2022-10-12 07:08:35 +0200

    Made the Navbar component responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import { FiMenu, FiX } from "react-icons"
+import React, { useState } from 'react';
+import { FiMenu, FiX } from "react-icons/fi"
 
 // Importing the style file
 import '../css/Navbar.css'
 
 // Creating the Navbar component
 const Navbar = ({ navbarLinks}) => {
+
+    const [toggleMenu, setToggleMenu] = useState(false);
+
+    // Function that will toogle the menu open or closed
+    const onMenuToggle = () => {
+        setToggleMenu(!toggleMenu)
+    }
+
     return (
         <nav className='navbar'>
             <span className="logo">Travel</span>
-            <ul className='navbar-list'>{navbarLinks.map(navlink => {
+            {toggleMenu ? <FiX size={25} className="navbar-icon" onClick={onMenuToggle} /> : <FiMenu size={25} className="navbar-icon" onClick={onMenuToggle} />}
+            <ul className={`navbar-list ${toggleMenu && "navbar-list-active"}`}>{navbarLinks.map(navlink => {
                 return <li className='navbar-item'>
                     <a href={navlink.url}>{navlink.title}</a>
                 </li>
